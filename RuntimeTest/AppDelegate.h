7e9c2a480382f6add9191cc4a632f66d3f860259
朱一天 <zhuyitian@daoyan-2.local>
2017-12-26 15:00:47 +0800

    Initial Commit

---
+++ RuntimeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeTest
+//
+//  Created by 朱一天 on 2017/12/26.
+//  Copyright © 2017年 朱一天. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeTest
+//
+//  Created by 朱一天 on 2017/12/26.
+//  Copyright © 2017年 朱一天. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
